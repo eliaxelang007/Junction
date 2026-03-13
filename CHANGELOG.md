5cb03dc6c2b0223fe570d30af1853ccfe79e23cd
Eli <eli.axel.ang@gmail.com>
2026-03-13 17:10:28 +0800

    Something must work, right?

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
-## 1.0.0
+# 1.0.2
 
-- Initial version.
+- This might be working!
 
 # 1.0.1
 
 - Migrated from being a Dart library to being a Flutter library. 
+
+## 1.0.0
+
+- Initial version.
--- lib/src/junction_base.dart
+++ lib/src/junction_base.dart
@@ -1,15 +1,13 @@
-import 'package:result_type/result_type.dart';
+import "package:flutter/foundation.dart";
 import "package:path/path.dart" as p;
-import "dart:typed_data";
 import "dart:convert";
-import "dart:io";
-import "package:cross_file/cross_file.dart";
 import "package:file_selector/file_selector.dart";
 
 sealed class CrossFilesystemData {}
 
 typedef CrossFilesystemItem<Data extends CrossFilesystemData> =
     MapEntry<CrossFilesystemName, Data>;
+typedef AnyCrossFilesystemItem = CrossFilesystemItem<CrossFilesystemData>;
 
 class CrossFileData extends CrossFilesystemData {
   final Uint8List bytes;
@@ -61,38 +59,58 @@ enum CrossPathError implements Exception {
 
 extension type CrossPath._(List<CrossFilesystemName> pathElements)
     implements List<CrossFilesystemName> {
-  static Result<(), CrossPathError> _validate(List<CrossFilesystemName> path) {
+  static CrossPathError? _validate(List<CrossFilesystemName> path) {
     if (path.isEmpty) {
-      return Failure(CrossPathError.empty);
+      return CrossPathError.empty;
     }
 
-    return Success(());
+    return null;
   }
 
-  static Result<CrossPath, CrossPathError> create(
-    List<CrossFilesystemName> path,
-  ) {
-    return _validate(path).map((_) => CrossPath._(path));
+  factory CrossPath(List<CrossFilesystemName> path) {
+    final error = _validate(path);
+
+    if (error != null) {
+      throw error;
+    }
+
+    return CrossPath._(path);
   }
 
   String asString() {
     return p.joinAll(pathElements);
   }
+
+  static CrossPath fromString(String maybePath) {
+    return CrossPath(
+      p
+          .split(maybePath)
+          .map((maybePart) => CrossFilesystemName(maybePart))
+          .toList(),
+    );
+  }
 }
 
 /* + Main API + */
 
-enum CrossReadError implements Exception {} // TODO
+/// This is a version of assert that doesn't get disabled in production.
+/// See https://dart.dev/language/error-handling#assert
+void _enforce(bool condition, [String? message]) {
+  if (condition) {
+    return;
+  }
+  throw AssertionError(message);
+}
 
 sealed class CrossReadHandle {
-  Future<Result<CrossFilesystemItem, CrossReadError>> read();
+  Future<AnyCrossFilesystemItem> read();
 }
 
 // enum CrossReadHandleError implements Exception {} // TODO
 
 /// Normally, the user will only interact with [CrossReadHandleRequest] through [showOpenFilePicker].
-/// However, if a native user just wanted to get a file path from [showOpenFilePicker],
-/// They could just write
+/// However, if you just wanted to get a file path from [showOpenFilePicker] on a native platform,
+/// You can just write
 ///
 /// ```
 /// CrossReadHandleRequest request = showOpenFilePicker();
@@ -104,128 +122,242 @@ sealed class CrossReadHandle {
 ///
 /// Which abstracts away the platform most of the time but still allows for specific platform handling!
 sealed class CrossReadHandleRequest {
+  /// See [NativeReadHandleRequest.handle]'s documentation for why this needs to be async.
   Future<CrossReadHandle> handle();
 }
 
-/// [showOpenFilePicker](https://developer.mozilla.org/en-US/docs/Web/API/Window/showOpenFilePicker)
-/// already gives you a file handle on web, so a [WebHandleRequest] is just a newtype wrapper for it
-/// that implements [CrossReadHandleRequest].
-class WebHandleRequest extends CrossReadHandleRequest {
-  final WebReadHandle
-  webHandle; // The web's [showOpenFilePicker] function already gives us a file handle, so the request object just stores it for later.
+class WebReadHandleRequest extends CrossReadHandleRequest {
+  /// The web's [showOpenFilePicker](https://developer.mozilla.org/en-US/docs/Web/API/Window/showOpenFilePicker) function already gives us a file handle!
+  /// When [WebReadHandle] isn't just a wrapper for [XFile] anymore, [WebReadHandleRequest] should just store the file handle for later.
+  final WebReadHandle webHandle;
 
-  WebHandleRequest({required this.webHandle});
-
-  @override
-  Future<CrossReadHandle> handle() {
-    return Future.syncValue(webHandle);
+  WebReadHandleRequest({required this.webHandle}) {
+    _enforce(
+      kIsWeb,
+      "You can only instantiate this type from a web environment!",
+    );
   }
-}
 
-class NativeHandleRequest extends CrossReadHandleRequest {
-  final CrossPath path;
-
-  NativeHandleRequest({required this.path});
-
+  /// See [NativeReadHandleRequest.handle]'s documentation for why this needs to be async.
   @override
   Future<CrossReadHandle> handle() async {
-    final stringPath = path.asString();
-    final type = await FileSystemEntity.type(stringPath);
-
-    final filesystemEntity = switch (type) {
-      FileSystemEntityType.file => File(stringPath),
-      FileSystemEntityType.directory => Directory(stringPath),
-      FileSystemEntityType.notFound => throw FileSystemException(
-        'Entity not found',
-        stringPath,
-      ),
-      _ => throw UnsupportedError('Unsupported file system entity type'),
-    };
-
-    return NativeReadHandle(entity: filesystemEntity);
+    return webHandle;
   }
 }
 
-Future<CrossReadHandleRequest> showOpenFilePicker({
-    String? label,
-    List<String>? allowedExtensions,
-    List<String>? uniformTypeIdentifiers,
-    List<String>? webWildCards,
-  }) {
-  XTypeGroup typeGroup = XTypeGroup(
-    label: label,
-    extensions: allowedExtensions,
-    uniformTypeIdentifiers: uniformTypeIdentifiers,
-    webWildCards: webWildCards
+class NativeReadHandleRequest extends CrossReadHandleRequest {
+  /// An object that you can use to request for a read handle to a file? That's a file path!
+  /// Right now, it still contains a [NativeReadHandle] (which is an [XFile] internally), but later on,
+  /// this should just be a newtype wrapper for [CrossPath].
+  final NativeReadHandle path;
+
+  NativeReadHandleRequest({required this.path}) {
+    _enforce(
+      !kIsWeb,
+      "You can only instantiate this type from a native environment!",
+    );
+  }
+
+  /// Later on, this function should give a file handle to the file path.
+  /// This involves [FileSystemEntity.type], which is why it has to be sync even though it's async.
+  @override
+  Future<CrossReadHandle> handle() async {
+    return path;
+  }
+}
+
+Future<CrossReadHandleRequest?> showOpenFilePicker({
+  String? label,
+  List<String>? allowedExtensions,
+  List<String>? uniformTypeIdentifiers,
+  List<String>? webWildCards,
+}) async {
+  final handle = await openFile(
+    acceptedTypeGroups: <XTypeGroup>[
+      XTypeGroup(
+        label: label,
+        extensions: allowedExtensions,
+        uniformTypeIdentifiers: uniformTypeIdentifiers,
+        webWildCards: webWildCards,
+      ),
+    ],
   );
 
-  if (kIsWeb)
-  
-  await openFile(
-    acceptedTypeGroups: <XTypeGroup>[typeGroup],
-  );
+  if (handle == null) return null;
+
+  return (kIsWeb)
+      ? WebReadHandleRequest(webHandle: WebReadHandle(webHandle: handle))
+      : NativeReadHandleRequest(path: NativeReadHandle(nativeHandle: handle));
 } // TODO
 
 class WebReadHandle extends CrossReadHandle {
+  /// This is supposed to be a [FileSystemFileHandle](https://developer.mozilla.org/en-US/docs/Web/API/FileSystemFileHandle)!
+  /// But for now, it's an [XFile].
+  final XFile webHandle;
+
+  WebReadHandle({required this.webHandle}) {
+    _enforce(
+      kIsWeb,
+      "You can only instantiate this type from a web environment!",
+    );
+  }
+
+  /// Because of cross_file's design philosophy, [AnyCrossFilesystemItem] will never be a directory.
+  /// Later on, it should be able to support that use case!
   @override
-  Future<Result<CrossFilesystemItem<CrossFilesystemData>, CrossReadError>>
-  read() {
-    // TODO: implement read
-    throw UnimplementedError();
+  Future<AnyCrossFilesystemItem> read() async {
+    return CrossFile(
+      name: CrossFilesystemName(webHandle.name),
+      data: CrossFileData(bytes: await webHandle.readAsBytes()),
+    );
   }
 }
 
 class NativeReadHandle extends CrossReadHandle {
-  final FileSystemEntity entity;
+  /// This is supposed to be a [FileSystemEntity]!
+  /// But for now, it's an [XFile].
+  final XFile nativeHandle;
 
-  NativeReadHandle({required this.entity});
+  NativeReadHandle({required this.nativeHandle}) {
+    _enforce(
+      !kIsWeb,
+      "You can only instantiate this type from a native environment!",
+    );
+  }
 
   @override
-  Future<Result<CrossFilesystemItem<CrossFilesystemData>, CrossReadError>>
-  read() {
-    // TODO: implement read
-    throw UnimplementedError();
+  Future<AnyCrossFilesystemItem> read() async {
+    return CrossFile(
+      name: CrossFilesystemName(nativeHandle.name),
+      data: CrossFileData(bytes: await nativeHandle.readAsBytes()),
+    );
   }
 }
 
+// enum CrossWriteError implements Exception { TODO } // TODO
+
 sealed class CrossWriteHandleRequest {
-  Future<Result<CrossWriteHandle>> handle();
+  /// See [WebWriteHandleRequest.handle]'s documentation for why this needs to be async!
+  Future<CrossWriteHandle> handle();
 }
 
-class WebWriteHandleRequest extends CrossWriteHandleRequest {}
+/// On the web, you can't choose where to write/download your files, so this is really just a type for show.
+class WebWriteHandleRequest extends CrossWriteHandleRequest {
+  WebWriteHandleRequest() {
+    _enforce(
+      kIsWeb,
+      "You can only instantiate this type from a web environment!",
+    );
+  }
+
+  /// [FileSystemFileHandle.createWritable](https://developer.mozilla.org/en-US/docs/Web/API/FileSystemFileHandle/createWritable) is async.
+  /// Therefore, handle needs to be async.
+  @override
+  Future<CrossWriteHandle> handle() async {
+    return WebWriteHandle();
+  }
+}
 
 class NativeWriteHandleRequest extends CrossWriteHandleRequest {
+  /// An object that you can use to request for a write handle to a file? That's a file path!
   final CrossPath writeTo;
+
+  /// Right now, it still contains a [NativeWriteHandle] (which is an [XFile] internally), but later on,
+  /// this should just be a newtype wrapper for [CrossPath].
+
+  NativeWriteHandleRequest({required this.writeTo}) {
+    _enforce(
+      !kIsWeb,
+      "You can only instantiate this type from a native environment!",
+    );
+  }
+
+  /// See [WebWriteHandleRequest.handle]'s documentation for why this needs to be async!
+  @override
+  Future<CrossWriteHandle> handle() async {
+    return NativeWriteHandle(writeTo: writeTo);
+  }
 }
 
-CrossWriteHandleRequest showSaveFilePicker() {}
+/// Because of the limitations of cross_path, I can only have [showSaveFilePicker] return [NativeWriteHandleRequest]s.
+/// Why? Because calling [XFile.saveTo] on a cross_file on the web makes it open the file picker and save at the same time!
+/// The step of getting a write handle and then actually writing to it is only separable  on native.
+Future<NativeWriteHandleRequest?> showSaveFilePicker({
+  String? suggestedName,
+}) async {
+  final saveLocation = await getSaveLocation(suggestedName: suggestedName);
 
-enum CrossWriteError implements Exception {}
+  if (saveLocation == null) {
+    return null;
+  }
+
+  return NativeWriteHandleRequest(
+    writeTo: CrossPath.fromString(saveLocation.path),
+  );
+}
 
 sealed class CrossWriteHandle {
-  Future<Result<(), CrossWriteError>> write(CrossFilesystemItem item);
+  Future<void> write(CrossFilesystemItem item);
 }
 
 class WebWriteHandle extends CrossWriteHandle {
+  WebWriteHandle() {
+    _enforce(
+      kIsWeb,
+      "You can only instantiate this type from a web environment!",
+    );
+  }
+
   @override
-  Future<Result<(), CrossWriteError>> write(
-    CrossFilesystemItem<CrossFilesystemData> item,
-  ) {
-    // TODO: implement write
-    throw UnimplementedError();
+  Future<void> write(AnyCrossFilesystemItem item) async {
+    final data = item.value;
+
+    if (data is! CrossFileData) {
+      _enforce(
+        false,
+        "Apologies! We can only write [CrossFile]s for now, but we're working towards being able to write [CrossFolder]s in the future, too.",
+      );
+
+      return;
+    }
+
+    /// The string here is "" because in actuality, you can't control where the file will be saved!
+    XFile.fromData(data.bytes).saveTo("");
   }
 }
 
-/// If someone wants to write a file to a specific folder on native, they can just
-/// [NativeWriteHandle(Path.fromString("./some/file/location").unwrap()).write(file);]
-/// Which would throw an unsupported error on web.
+/// If you want to write a file to a specific folder on native, you could write something like:
+///
+/// ```
+/// await (
+///   await NativeWriteHandleRequest(writeTo: CrossPath.fromString("./some/file/location")).handle()
+/// ).write(file)
+/// ```
+///
+/// Which should throw an error on web because it's unsupported!
+///
+/// Also, this should just be a write handle to the file itself, but right now,
+/// it still stores a [CrossPath] within itself because it relies on cross_file.
+/// See [NativeWriteHandleRequest]'s documentation for more.
 class NativeWriteHandle extends CrossWriteHandle {
+  final CrossPath writeTo;
+
+  NativeWriteHandle({required this.writeTo}) : assert(!kIsWeb);
+
   @override
-  Future<Result<(), CrossWriteError>> write(
-    CrossFilesystemItem<CrossFilesystemData> item,
-  ) {
-    // TODO: implement write
-    throw UnimplementedError();
+  Future<void> write(AnyCrossFilesystemItem item) async {
+    final data = item.value;
+
+    if (data is! CrossFileData) {
+      _enforce(
+        false,
+        "Apologies! We can only write [CrossFile]s for now, but we're working towards being able to write [CrossFolder]s in the future, too.",
+      );
+
+      return;
+    }
+
+    XFile.fromData(data.bytes).saveTo(writeTo.asString());
   }
 }
 
@@ -257,25 +389,25 @@ enum CrossFilesystemNameError implements Exception {
 
 extension type const CrossFilesystemName._(String fullFilename)
     implements String {
-  static Result<(), CrossFilesystemNameError> _validate(String fullFilename) {
+  static CrossFilesystemNameError? _validate(String fullFilename) {
     if (fullFilename.isEmpty) {
-      return Failure(CrossFilesystemNameError.empty);
+      return CrossFilesystemNameError.empty;
     }
 
     if (fullFilename.length > maxPath) {
-      return Failure(CrossFilesystemNameError.exceedingLength);
+      return CrossFilesystemNameError.exceedingLength;
     }
 
     if (fullFilename.endsWith('.')) {
-      return Failure(CrossFilesystemNameError.endingPeriod);
+      return CrossFilesystemNameError.endingPeriod;
     }
 
     // if (fullFilename.codeUnits.any((c) => c < 32 || c > 126)) {
-    //   return Failure(CrossFilesystemNameError.nonAscii);
+    //   return CrossFilesystemNameError.nonAscii;
     // }
 
     if (fullFilename.contains(RegExp(r'[<>:"/\\|?*]'))) {
-      return Failure(CrossFilesystemNameError.forbiddenCharacters);
+      return CrossFilesystemNameError.forbiddenCharacters;
     }
 
     // if (fullFilename != fullFilename.toLowerCase()) {
@@ -283,17 +415,19 @@ extension type const CrossFilesystemName._(String fullFilename)
     // }
 
     if (fullFilename.trim().length != fullFilename.length) {
-      return Failure(CrossFilesystemNameError.beginningOrEndingWithWhitespace);
+      return CrossFilesystemNameError.beginningOrEndingWithWhitespace;
     }
 
-    return Success(());
+    return null;
   }
 
-  static Result<CrossFilesystemName, CrossFilesystemNameError> create(
-    String fullFilename,
-  ) {
-    return _validate(
-      fullFilename,
-    ).map((_) => CrossFilesystemName._(fullFilename));
+  factory CrossFilesystemName(String fullFilename) {
+    final error = _validate(fullFilename);
+
+    if (error != null) {
+      throw error;
+    }
+
+    return CrossFilesystemName._(fullFilename);
   }
 }
